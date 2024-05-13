450616e331257f2ee80281f6d57d2db097d39b14
CelestineOkpataku <celestine.okpataku@gmail.com>
2024-05-13 12:37:20 -0500

    create full eks cluster

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "celestine-okpataku-remote-backend"
+    key            = "okpataku/comcast-assessment/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "dynamodb-state-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
