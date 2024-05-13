4eeed0d16394cdfaca32610e479f9ed5b7a31eae
CelestineOkpataku <celestine.okpataku@gmail.com>
2024-05-13 18:46:18 -0500

    trying

--- backend.tf
+++ backend.tf
@@ -4,5 +4,8 @@ terraform {
     key            = "okpataku/comcast-assessment/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "dynamodb-state-locking"
+    assume_role = {
+      
+    }
   }
 }
