657e8ee0d5b88da3d80fcf6398b9ecc8ad86d5cb
CelestineOkpataku <celestine.okpataku@gmail.com>
2024-05-13 18:58:29 -0500

    200

--- providers.tf
+++ providers.tf
@@ -1,18 +1,4 @@
 
-
-provider "kubernetes" {
-    host = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-
-    exec {
-        api_version = "client.authentication.k8s.io/v1"
-        command = "aws"
-        #This requires awscli to be installed locally where Terraform is executed
-
-        args = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-    }
-}
-
 terraform {
     required_providers {
         aws = {
